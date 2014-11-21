d68e58ec3afc2c9ad6af7e5f6f9b29cba51b1e45
Szilard Huber <szilard.huber@gmail.com>
2014-11-21 21:03:43 +0100

    Texts are stored in persistent storage. Scheduler starts to do something.

--- src/forgetmenot.c
+++ src/forgetmenot.c
@@ -1,8 +1,19 @@
 #include <pebble.h>
 #include <time.h>
 
+#define MINUTE 60
+#define HOUR 60 * MINUTE
+#define DAY 24 * HOUR
+
 static Window *window;
 static TextLayer *text_layer;
+static size_t notification_interval = 1 * DAY;
+// static size_t notification_interval = 5;
+
+// forward declarations
+static void schedule(int32_t id, const char* message);
+
+// handlers
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(text_layer, "Select");
@@ -27,37 +38,83 @@ static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button");
+  text_layer = text_layer_create((GRect) { .origin = { 0, 12 }, .size = { bounds.size.w / 3 * 2, bounds.size.h } });
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+  text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_overflow_mode(text_layer, GTextOverflowModeTrailingEllipsis);
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
+}
+
+static void notify(const char* message) {
+    light_enable_interaction();
+    vibes_short_pulse();
+    text_layer_set_text(text_layer, message);
 }
 
 static void window_unload(Window *window) {
   text_layer_destroy(text_layer);
 }
 
+void set_message(int32_t cookie, const char * message)
+{
+    persist_write_string( cookie, message);
+}
+
+
+void get_message(int32_t cookie, char * message, const size_t buffer_size)
+{
+    persist_read_string(cookie, message, buffer_size);
+}
+
 static void on_wakeup(int32_t wakeupid, int32_t cookie)
 {
     APP_LOG(APP_LOG_LEVEL_DEBUG, "App woke up, wakeupid: %ld, cookie: %ld", wakeupid, cookie);
+    char * message = (char *)malloc(128);
+    get_message(cookie, message, 128);
+    schedule(wakeupid, message);
+    notify(message);
+    free(message);
 }
 
-static void init(void) {
-  window = window_create();
-  window_set_click_config_provider(window, click_config_provider);
-  window_set_window_handlers(window, (WindowHandlers) {
-    .load = window_load,
-    .unload = window_unload,
-  });
-  const bool animated = true;
-  wakeup_service_subscribe(on_wakeup);
-  time_t now;
-  time(&now);
-  if (wakeup_schedule(now + 60, 42, true) < 0)
-  {
-      APP_LOG(APP_LOG_LEVEL_DEBUG, "Failed to schedule wakeup");
-  }
-  window_stack_push(window, animated);
+static void schedule(int32_t id, const char* message)
+{
+    time_t now;
+    time(&now);
+    int32_t wakeup_id = wakeup_schedule(now + notification_interval, id, true);
+    if (wakeup_id < 0)
+    {
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "Failed to schedule wakeup. Reason: %ld", id);
+    }
+    else
+    {
+        set_message(id, message);
+    }
+}
+
+static void init(void)
+{
+    wakeup_service_subscribe(on_wakeup);
+    window = window_create();
+    window_set_click_config_provider(window, click_config_provider);
+    window_set_window_handlers(window, (WindowHandlers) {
+        .load = window_load,
+        .unload = window_unload,
+    });
+    const bool animated = true;
+    window_stack_push(window, animated);
+    if (launch_reason() == APP_LAUNCH_WAKEUP)
+    {
+        int32_t wakeup_id;
+        int32_t cookie;
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "App started due to wakeup event");
+        wakeup_get_launch_event(&wakeup_id, &cookie);
+        on_wakeup(wakeup_id, cookie);
+    }
+    else
+    {
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "Scheduling wakeup");
+        schedule(42, "Did you take your pills?");
+    }
 }
 
 static void deinit(void) {
