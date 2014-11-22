f2e3f5f2517e82d63d7acec9baf88570e80db21d
Szilard Huber <szilard.huber@gmail.com>
2014-11-22 16:20:28 +0100

    Fixed typos.

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,8 +1,9 @@
 function init(e) {
-    Pebble.addEventListener('showConfiguration')
+    Pebble.addEventListener('showConfiguration', function(e)
     {
         console.log('Setting configuration event listener');
-    }
+        Pebble.openURL("https://raw.githubusercontent.com/szilardhuber/pebble-forgetmenot/master/src/js/settings.html");
+    });
 }
 
 Pebble.addEventListener('ready', init);
