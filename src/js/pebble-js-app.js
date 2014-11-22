f6c763f4c6f14b39572fd02457435dca6460be19
Szilard Huber <szilard.huber@gmail.com>
2014-11-22 16:24:11 +0100

    Found the url for pebble settings.

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -2,7 +2,7 @@ function init(e) {
     Pebble.addEventListener('showConfiguration', function(e)
     {
         console.log('Setting configuration event listener');
-        Pebble.openURL("https://raw.githubusercontent.com/szilardhuber/pebble-forgetmenot/master/src/js/settings.html");
+        Pebble.openURL("http://szilardhuber.github.io/pebble-forgetmenot/");
     });
 }
 
